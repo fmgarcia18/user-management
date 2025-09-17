3435f53bf6f30cf97c408ecbcd91e93d8c144a4a
fmgarcia18 <fmgarcia18@gmail.com>
2025-09-17 18:22:23 -0300

    primer commit configuracion del proyecto

---
+++ src/main/java/com/fmgarcia/user/management/FmgarciaUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmgarciaUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmgarciaUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmgarcia/user/management/FmgarciaUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmgarcia.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmgarciaUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
